7d08bc34fe51fef2d2d16e9be719e073cc742b8c
wtasg <225132696+wtasg@users.noreply.github.com>
2025-11-09 11:26:55 +0530

    2025.11.09 done

--- gilded_rose.js
+++ gilded_rose.js
@@ -15,28 +15,25 @@ class Shop {
             if (name === 'Sulfuras, Hand of Ragnaros') {
                 return new Item(name, sellIn, quality);
             }
+            const maxQ = Math.max(50, quality);
 
             if (name === "Aged Brie") {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
+                quality = Math.min(maxQ, quality + 1);
+
                 sellIn = sellIn - 1;
-                if (sellIn < 0 && quality < 50) {
-                    quality = quality + 1;
+                if (sellIn < 0) {
+                    quality = Math.min(maxQ, quality + 1);
                 }
                 return new Item(name, sellIn, quality);
             }
 
             if (name === 'Backstage passes to a TAFKAL80ETC concert') {
-                if (quality < 50) {
-                    let limit = Math.max(50, quality);
-                    quality = Math.min(limit, quality + 1);
-                    if (sellIn < 11) {
-                        quality = Math.min(limit, quality + 1);
-                    }
-                    if (sellIn < 6) {
-                        quality = Math.min(limit, quality + 1);
-                    }
+                quality = Math.min(maxQ, quality + 1);
+                if (sellIn < 11) {
+                    quality = Math.min(maxQ, quality + 1);
+                }
+                if (sellIn < 6) {
+                    quality = Math.min(maxQ, quality + 1);
                 }
                 sellIn = sellIn - 1;
                 if (sellIn < 0) {
@@ -45,7 +42,7 @@ class Shop {
                 return new Item(name, sellIn, quality);
             }
 
-            // rest of the items
+            // rest of the items get their quality decreased
             quality = Math.max(0, quality - 1);
             sellIn = sellIn - 1;
             if (sellIn < 0) {
