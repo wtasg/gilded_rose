9a460b89f47428ace9ded48e884766306e80a818
wtasg <225132696+wtasg@users.noreply.github.com>
2025-11-10 10:18:44 +0530

    2025.11.10

--- nodejs/gilded_rose.js
+++ nodejs/gilded_rose.js
@@ -15,39 +15,23 @@ class Shop {
             if (name === 'Sulfuras, Hand of Ragnaros') {
                 return new Item(name, sellIn, quality);
             }
-            const maxQ = Math.max(50, quality);
-
-            if (name === "Aged Brie") {
-                quality = Math.min(maxQ, quality + 1);
-
+            if (name === 'Aged Brie') {
+                quality += quality < 50 ? 1 : 0;
                 sellIn = sellIn - 1;
-                if (sellIn < 0) {
-                    quality = Math.min(maxQ, quality + 1);
-                }
+                quality += sellIn < 0 && quality < 50 ? 1 : 0;
                 return new Item(name, sellIn, quality);
             }
-
             if (name === 'Backstage passes to a TAFKAL80ETC concert') {
-                quality = Math.min(maxQ, quality + 1);
-                if (sellIn < 11) {
-                    quality = Math.min(maxQ, quality + 1);
-                }
-                if (sellIn < 6) {
-                    quality = Math.min(maxQ, quality + 1);
-                }
+                quality += quality < 50 ? 1 : 0;
+                quality += sellIn < 11 && quality < 50 ? 1 : 0;
+                quality += sellIn < 6 && quality < 50 ? 1 : 0;
                 sellIn = sellIn - 1;
-                if (sellIn < 0) {
-                    quality = 0;
-                }
+                quality = sellIn < 0 ? 0 : quality;
                 return new Item(name, sellIn, quality);
             }
-
-            // rest of the items get their quality decreased
-            quality = Math.max(0, quality - 1);
+            quality -= quality > 0 ? 1 : 0;
             sellIn = sellIn - 1;
-            if (sellIn < 0) {
-                quality = Math.max(0, quality - 1);
-            }
+            quality -= sellIn < 0 && quality > 0 ? 1 : 0;
             return new Item(name, sellIn, quality);
         });
     }
